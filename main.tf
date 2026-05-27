8b0eec68a37495e4f6d9f1c040dbd5a55deb57da
bokang03 <bogang12345@gmail.com>
2026-05-27 14:42:21 +0900

    도커 네트워크 생성 및 npm, redis, mysql 설정 추가

--- main.tf
+++ main.tf
@@ -1,67 +1,330 @@
-// 테라폼 설정의 시작
 terraform {
-  // 필요한 프로바이더(클라우드 서비스 제공자)를 설정
+  // aws 라이브러리 불러옴
   required_providers {
-    // AWS 프로바이더를 사용한다고 선언
     aws = {
-      // AWS 프로바이더의 출처를 hashicorp/aws로 지정
       source = "hashicorp/aws"
     }
   }
 }
 
-// AWS를 제공자로 사용한다고 선언
+# AWS 설정 시작
 provider "aws" {
-  // AWS에서 사용할 리전을 변수로부터 받아옴
   region = var.region
 }
+# AWS 설정 끝
 
-// AWS VPC(Virtual Private Cloud) 리소스를 생성하고 이름을 'vpc_1'로 설정
+# VPC 설정 시작
 resource "aws_vpc" "vpc_1" {
-  // VPC의 IP 주소 범위를 설정
   cidr_block = "10.0.0.0/16"
 
-  // DNS 지원을 활성화
-  enable_dns_support = true
-  // DNS 호스트 이름 지정을 활성화
+  enable_dns_support   = true
   enable_dns_hostnames = true
 
-  // 리소스에 대한 태그를 설정
   tags = {
     Name = "${var.prefix}-vpc-1"
   }
 }
 
-// AWS 서브넷 리소스를 생성하고 이름을 'subnet_1'로 설정
 resource "aws_subnet" "subnet_1" {
-  // 이 서브넷이 속할 VPC를 지정. 여기서는 'vpc_1'를 선택
-  vpc_id = aws_vpc.vpc_1.id
-  // 서브넷의 IP 주소 범위를 설정
-  cidr_block = "10.0.1.0/24"
-  // 서브넷이 위치할 가용 영역을 설정
-  availability_zone = "${var.region}a"
-  // 이 서브넷에 배포되는 인스턴스에 공용 IP를 자동으로 할당
+  vpc_id                  = aws_vpc.vpc_1.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "${var.region}a"
   map_public_ip_on_launch = true
 
-  // 리소스에 대한 태그를 설정
   tags = {
     Name = "${var.prefix}-subnet-1"
   }
 }
 
-// AWS 서브넷 리소스를 생성하고 이름을 'subnet_2'로 설정
 resource "aws_subnet" "subnet_2" {
-  // 이 서브넷이 속할 VPC를 지정. 여기서는 'vpc_1'를 선택
-  vpc_id = aws_vpc.vpc_1.id
-  // 서브넷의 IP 주소 범위를 설정
-  cidr_block = "10.0.2.0/24"
-  // 서브넷이 위치할 가용 영역을 설정
-  availability_zone = "${var.region}b"
-  // 이 서브넷에 배포되는 인스턴스에 공용 IP를 자동으로 할당
+  vpc_id                  = aws_vpc.vpc_1.id
+  cidr_block              = "10.0.2.0/24"
+  availability_zone       = "${var.region}b"
   map_public_ip_on_launch = true
 
-  // 리소스에 대한 태그를 설정
   tags = {
     Name = "${var.prefix}-subnet-2"
   }
+}
+
+resource "aws_subnet" "subnet_3" {
+  vpc_id                  = aws_vpc.vpc_1.id
+  cidr_block              = "10.0.3.0/24"
+  availability_zone       = "${var.region}c"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "${var.prefix}-subnet-3"
+  }
+}
+
+resource "aws_internet_gateway" "igw_1" {
+  vpc_id = aws_vpc.vpc_1.id
+
+  tags = {
+    Name = "${var.prefix}-igw-1"
+  }
+}
+
+resource "aws_route_table" "rt_1" {
+  vpc_id = aws_vpc.vpc_1.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw_1.id
+  }
+
+  tags = {
+    Name = "${var.prefix}-rt-1"
+  }
+}
+
+resource "aws_route_table_association" "association_1" {
+  subnet_id      = aws_subnet.subnet_1.id
+  route_table_id = aws_route_table.rt_1.id
+}
+
+resource "aws_route_table_association" "association_2" {
+  subnet_id      = aws_subnet.subnet_2.id
+  route_table_id = aws_route_table.rt_1.id
+}
+
+resource "aws_route_table_association" "association_3" {
+  subnet_id      = aws_subnet.subnet_3.id
+  route_table_id = aws_route_table.rt_1.id
+}
+
+resource "aws_security_group" "sg_1" {
+  name = "${var.prefix}-sg-1"
+
+  ingress {
+    from_port = 0
+    to_port   = 0
+    protocol  = "all"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 0
+    to_port   = 0
+    protocol  = "all"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  vpc_id = aws_vpc.vpc_1.id
+
+  tags = {
+    Name = "${var.prefix}-sg-1"
+  }
+}
+
+# EC2 설정 시작
+
+# EC2 역할 생성
+resource "aws_iam_role" "ec2_role_4" {
+  name = "${var.prefix}-ec2-role-4"
+
+  # 이 역할에 대한 신뢰 정책 설정. EC2 서비스가 이 역할을 가정할 수 있도록 설정
+  assume_role_policy = <<EOF
+  {
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Sid": "",
+        "Action": "sts:AssumeRole",
+        "Principal": {
+            "Service": "ec2.amazonaws.com"
+        },
+        "Effect": "Allow"
+      }
+    ]
+  }
+  EOF
+}
+
+# EC2 역할에 AmazonS3FullAccess 정책을 부착
+resource "aws_iam_role_policy_attachment" "s3_full_access" {
+  role       = aws_iam_role.ec2_role_4.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
+}
+
+# EC2 역할에 AmazonEC2RoleforSSM 정책을 부착
+resource "aws_iam_role_policy_attachment" "ec2_ssm" {
+  role       = aws_iam_role.ec2_role_4.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2RoleforSSM"
+}
+
+# IAM 인스턴스 프로파일 생성
+resource "aws_iam_instance_profile" "instance_profile_3" {
+  name = "${var.prefix}-instance-profile-3"
+  role = aws_iam_role.ec2_role_4.name
+}
+
+locals {
+  ec2_user_data_base = <<-END_OF_FILE
+#!/bin/bash
+# 가상 메모리 4GB 설정
+sudo dd if=/dev/zero of=/swapfile bs=128M count=32
+sudo chmod 600 /swapfile
+sudo mkswap /swapfile
+sudo swapon /swapfile
+sudo sh -c 'echo "/swapfile swap swap defaults 0 0" >> /etc/fstab'
+
+# 도커 설치 및 실행/활성화
+yum install docker -y
+systemctl enable docker
+systemctl start docker
+
+# 도커 네트워크 생성
+docker network create common
+
+# nginx 설치
+docker run -d \
+  --name npm_1 \
+  --restart unless-stopped \
+  --network common \
+  -p 80:80 \
+  -p 443:443 \
+  -p 81:81 \
+  -e TZ=Asia/Seoul \
+  -v /dockerProjects/npm_1/volumes/data:/data \
+  -v /dockerProjects/npm_1/volumes/etc/letsencrypt:/etc/letsencrypt \
+  jc21/nginx-proxy-manager:latest
+
+# redis 설치
+docker run -d \
+  --name=redis_1 \
+  --restart unless-stopped \
+  --network common \
+  -p 6379:6379 \
+  -e TZ=Asia/Seoul \
+  redis --requirepass lldj123414
+
+# mysql 설치
+docker run -d \
+  --name mysql_1 \
+  --restart unless-stopped \
+  -v /dockerProjects/mysql_1/volumes/var/lib/mysql:/var/lib/mysql \
+  -v /dockerProjects/mysql_1/volumes/etc/mysql/conf.d:/etc/mysql/conf.d \
+  --network common \
+  -p 3306:3306 \
+  -e MYSQL_ROOT_PASSWORD=lldj123414 \
+  -e TZ=Asia/Seoul \
+  mysql:latest
+
+# MySQL 컨테이너가 준비될 때까지 대기
+echo "MySQL이 기동될 때까지 대기 중..."
+until docker exec mysql_1 mysql -uroot -plldj123414 -e "SELECT 1" &> /dev/null; do
+  echo "MySQL이 아직 준비되지 않음. 5초 후 재시도..."
+  sleep 5
+done
+echo "MySQL이 준비됨. 초기화 스크립트 실행 중..."
+
+docker exec mysql_1 mysql -uroot -plldj123414 -e "
+CREATE USER 'lldjlocal'@'127.0.0.1' IDENTIFIED WITH caching_sha2_password BY '1234';
+CREATE USER 'lldjlocal'@'172.18.%.%' IDENTIFIED WITH caching_sha2_password BY '1234';
+CREATE USER 'lldj'@'%' IDENTIFIED WITH caching_sha2_password BY 'lldj123414';
+
+GRANT ALL PRIVILEGES ON *.* TO 'lldjlocal'@'127.0.0.1';
+GRANT ALL PRIVILEGES ON *.* TO 'lldjlocal'@'172.18.%.%';
+GRANT ALL PRIVILEGES ON *.* TO 'lldj'@'%';
+
+CREATE DATABASE glog_prod;
+
+FLUSH PRIVILEGES;
+"
+
+END_OF_FILE
+}
+
+data "aws_ssm_parameter" "amazon_linux_ami" {
+  name = "/aws/service/ami-amazon-linux-latest/al2023-ami-kernel-default-x86_64"
+}
+
+# EC2 인스턴스 생성
+resource "aws_instance" "ec2_1" {
+  # 사용할 AMI ID
+  ami = data.aws_ssm_parameter.amazon_linux_ami.value
+  # EC2 인스턴스 유형
+  instance_type = "t3.micro"
+  # 사용할 서브넷 ID
+  subnet_id = aws_subnet.subnet_2.id
+  # 적용할 보안 그룹 ID
+  vpc_security_group_ids = [aws_security_group.sg_1.id]
+  # 퍼블릭 IP 연결 설정
+  associate_public_ip_address = true
+
+  # 인스턴스에 IAM 역할 연결
+  iam_instance_profile = aws_iam_instance_profile.instance_profile_3.name
+
+  # 인스턴스에 태그 설정
+  tags = {
+    Name = "${var.prefix}-ec2-1"
+  }
+
+  # 루트 볼륨 설정
+  root_block_device {
+    volume_type = "gp3"
+    volume_size = 12 # 볼륨 크기를 12GB로 설정
+  }
+
+  user_data = <<-EOF
+${local.ec2_user_data_base}
+EOF
+}
+
+# EC2 인스턴스 생성
+resource "aws_instance" "ec2_2" {
+  # 사용할 AMI ID
+  ami = data.aws_ssm_parameter.amazon_linux_ami.value
+  # EC2 인스턴스 유형
+  instance_type = "t3.micro"
+  # 사용할 서브넷 ID
+  subnet_id = aws_subnet.subnet_2.id
+  # 적용할 보안 그룹 ID
+  vpc_security_group_ids = [aws_security_group.sg_1.id]
+  # 퍼블릭 IP 연결 설정
+  associate_public_ip_address = true
+
+  # 인스턴스에 IAM 역할 연결
+  iam_instance_profile = aws_iam_instance_profile.instance_profile_3.name
+
+  # 인스턴스에 태그 설정
+  tags = {
+    Name = "${var.prefix}-ec2-2"
+  }
+
+
+  # 루트 볼륨 설정
+  root_block_device {
+    volume_type = "gp3"
+    volume_size = 12 # 볼륨 크기를 12GB로 설정
+  }
+  user_data = <<-EOF
+${local.ec2_user_data_base}
+EOF
+}
+
+data "aws_eip" "eip_ec2_1" {
+  filter {
+    name   = "tag:EC2"
+    values = ["dev-ec2-1"]
+  }
+}
+
+data "aws_eip" "eip_ec2_2" {
+  filter {
+    name   = "tag:EC2"
+    values = ["dev-ec2-2"]
+  }
+}
+
+resource "aws_eip_association" "ec2_1" {
+  instance_id   = aws_instance.ec2_1.id
+  allocation_id = data.aws_eip.eip_ec2_1.id
+}
+
+resource "aws_eip_association" "ec2_2" {
+  instance_id   = aws_instance.ec2_2.id
+  allocation_id = data.aws_eip.eip_ec2_2.id
 }
